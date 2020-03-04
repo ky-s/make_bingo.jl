02eff645e400946f922568a2614b3fcacce87444
ky-s <kyohei_86@hotmail.com>
2020-03-04 23:23:55 +0900

    [clean] refatored

--- makebingo.jl
+++ makebingo.jl
@@ -1,17 +1,50 @@
-using Random
+using Random: shuffle
 
+# returns bingo card as 2-dim[5x5] Array.
+#
+# e.g.
+# ["3" "27" "35" "49" "70";
+#  "8" "28" "38" "46" "72";
+#  "1" "21" " "  "47" "63";
+#  "9" "30" "43" "60" "66";
+#  "5" "16" "32" "51" "62"]
 function makebingo()
-    bingo = map(i -> string.(shuffle(i:i+14)[1:5]), 1:15:75)
+    numbers_arr = [
+         1:14, # B
+        15:29, # I
+        30:44, # N
+        45:59, # G
+        60:74, # O
+    ]
 
+    bingo = map(numbers_arr) do numbers
+
+        shuffle(numbers)[1:5] .|> string
+
+    end
+
+    # center blank
     bingo[3][3] = " "
 
+    # 5-element Array{Array{String,1},1} => 5x5 Array{String,2}
     hcat(bingo...)
 end
 
 
 function output(bingo)
-    bingo_with_header = ["B" "I" "N" "G" "O"; bingo]
-    join(map(i -> join(lpad.(bingo_with_header[i, :], 2), " | "), 1:6), "\n")
+    bingo_with_header = [
+         "B" "I" "N" "G" "O";
+         bingo
+    ]
+
+    lines = map(1:6) do n
+        # left padding
+        padded = lpad.(bingo_with_header[n, :], 2)
+
+        join(padded, " | ")
+    end
+
+    join(lines, "\n")
 end
 
-println(output(makebingo()))
+(output ∘ makebingo)() |> println
